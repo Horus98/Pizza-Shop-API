58269f8f9d0f6b3326feb54d7a1a5b7c26ff1d58
Horus98 <42451015+Horus98@users.noreply.github.com>
2024-04-05 16:03:48 -0300

    Add coverage for all endpoints from Product Controller

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 insert into order_table (id, address, phone, time, email, date, total_cost, is_discount_applicable, state)
 values (10001, 'Sarmiento', '2920121212', '21:00', 'myemail@gmail.com', '2024-12-12', 1200, false, 'OPEN');
 
-insert into product (id, name, short_description, large_description, unit_price)
-values (10001, 'Honey', 'Es muy dulce', 'Proveniente del Asia, esta miel va a ser la mas dulce y rica que podras probar, es ideal para cocina o para endulzar tu vida misma', 120.12)
+//insert into product (id, name, short_description, large_description, unit_price)
+//values (10001, 'Honey', 'Es muy dulce', 'Proveniente del Asia, esta miel va a ser la mas dulce y rica que podras probar, es ideal para cocina o para endulzar tu vida misma', 120.12)
--- src/test/java/com/pizzashop/demo/controllers/ProductControllerIntegrationTest.java
+++ src/test/java/com/pizzashop/demo/controllers/ProductControllerIntegrationTest.java
@@ -1,10 +1,11 @@
 package com.pizzashop.demo.controllers;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pizzashop.demo.dataloaders.ProductDataLoader;
 import com.pizzashop.demo.entities.Product;
+import com.pizzashop.demo.repositories.ProductRepository;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,13 +13,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
@@ -28,14 +37,18 @@ public class ProductControllerIntegrationTest {
     @Autowired
     private ProductDataLoader dataLoader;
     @Autowired
+    private ProductRepository productRepository;
+    @Autowired
     private MockMvc mockMvc;
 
-    @BeforeEach
+    @BeforeAll
     public void setup() {
         dataLoader.loadTestData();
     }
 
     @Test
+    @Transactional
+    @Rollback
     public void givenProductId_whenGetProductById_thenReturnsProduct() throws Exception {
         Product product = (new ObjectMapper()).readValue(
                 mockMvc.perform(get("/products/{id}", 1L))
@@ -45,6 +58,74 @@ public class ProductControllerIntegrationTest {
                 .getResponse()
                 .getContentAsString(), Product.class
         );
-        assertTrue(product.equals(ProductDataLoader.getProduct(1L)));
+        assertEquals(product, ProductDataLoader.getProduct(1L));
+    }
+
+    @Test
+    @Rollback
+    @Transactional
+    public void testGetAllProductsEndpoint() throws Exception {
+        String responseBody = mockMvc.perform(get("/products"))
+                        .andExpect(status().isOk())
+                        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                        .andReturn()
+                        .getResponse()
+                .getContentAsString();
+        List<Product> productList = (new ObjectMapper()).readValue(responseBody, new TypeReference<List<Product>>() {});
+        List<Product> expectedList = List.of(ProductDataLoader.getProduct(1L), ProductDataLoader.getProduct(2L));
+        assertEquals(expectedList, productList);
+    }
+
+    @Test
+    @Transactional
+    @Rollback
+    public void testDeleteEndpoint() throws Exception {
+        mockMvc.perform(delete("/products/{id}", 1L))
+                .andExpect(status().isNoContent());
+
+        var isProductPresentInDb = productRepository.findById(1L).isPresent();
+
+        assertFalse(isProductPresentInDb);
+    }
+
+    @Test
+    @Transactional
+    @Rollback
+    public void testCreateEndpoint() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/products")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content((new ObjectMapper()).writeValueAsString(getNewProduct())))
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(3L))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("New"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.shortDescription").value("Short"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.largeDescription").value("Large Large Large Large Large Large Large Large Large"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.unitPrice").value(100f));
+    }
+
+    @Test
+    @Transactional
+    @Rollback
+    public void testUpdateEndpoint() throws Exception {
+        var product = getNewProduct();
+        mockMvc.perform(MockMvcRequestBuilders.put("/products/{id}", 1L)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content((new ObjectMapper()).writeValueAsString(getNewProduct())))
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(1L))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("New"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.shortDescription").value("Short"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.largeDescription").value("Large Large Large Large Large Large Large Large Large"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.unitPrice").value(100f));
+    }
+
+
+        private Product getNewProduct() {
+        var product = new Product();
+        product.setName("New");
+        product.setShortDescription("Short");
+        product.setLargeDescription("Large Large Large Large Large Large Large Large Large");
+        product.setUnitPrice(100f);
+        return product;
     }
 }
